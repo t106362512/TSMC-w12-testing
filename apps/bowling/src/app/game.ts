52cd2f236a9bfd68fdb27830ff0e0009da9f7f83
t106362512 <t106362512@gmail.com>
2022-05-11 20:19:21 +0800

    test(game): ✅ Add passed test 'all one'

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -1,7 +1,10 @@
 export class Game {
-  roll(pins: number) {}
+  private _score = 0;
+  roll(pins: number) {
+    this._score += pins;
+  }
 
   get score() {
-    return 0;
+    return this._score;
   }
 }
